ce27eac5974e2aa1042eb63c4c1cba8dc5769414
Dionis Gonzalez <dionis@mikmak.com>
2022-04-19 22:59:00 -0600

    adjusting isAdmin logic for AddProject page

--- src/pages/AddProject.js
+++ src/pages/AddProject.js
@@ -1,14 +1,13 @@
 import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 import ProjectInfo from './Project/ProjectInfo';
+import utils from 'services/utils';
 import 'styles/AddProject.css';
 
 export default function AddProject() {
-  const host = window.location.hostname;
-
   const [project, setProject] = useState({});
 
-  if (host !== 'localhost') return <Navigate to='/404' />
+  if (!utils.isAdmin) return <Navigate to='/404' />
 
   return (
     <article className='add-project'>
